aebda21e08652d15cca14c003915b27ade6cb5c7
vermaLucky <luckyverma1297@gmail.com>
2018-09-27 12:27:08 +0530

    loops in python

--- practice.py
+++ practice.py
@@ -38,7 +38,7 @@ output
 * * * * * 
 * * * * * 
 * * * * * 
-* * * * *"""
+* * * * *
 
 n=int(input("enter no of rows"))
 for i in range(1,n+1):
@@ -48,9 +48,33 @@ for i in range(1,n+1):
     print()
 
 
+y=int(input("ENTER THE NO OF ROWS"))
+for i in range(y,0,-1):
+    for j in range(1,i+1):
+        print(j,end=" ")
+
+    print()
+
+input=5
+
+output=
+ENTER THE NO OF ROWS5
+1 2 3 4 5 
+1 2 3 4 
+1 2 3 
+1 2 
+1 
 
 
+a=str(input("ENTER THE STRING"))
+l=len(a)
+for i in a:
+    if(i==a[l-1]):
+        l=l-1
+        continue
+    else:
+        print('no')
+        exit()
+print('yes')
 
-
-
-
+"""
